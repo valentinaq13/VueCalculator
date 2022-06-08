f238b511845a18c5e5e3ab0dc4d68e2a5fd08fc3
valentinaq13 <emeritavalentina@gmail.com>
2022-06-08 08:19:44 -0300

    operaciones matematicas

--- calculator/src/components/CalculatorComp.vue
+++ calculator/src/components/CalculatorComp.vue
@@ -6,19 +6,19 @@
         <div class="btn" @click="append(7)">7</div>
         <div class="btn" @click="append(8)">8</div>
         <div class="btn" @click="append(9)">9</div>
-        <div class="btn operator">/</div>
+        <div class="btn operator" @click="divide">/</div>
         <div class="btn" @click="append(4)">4</div>
         <div class="btn" @click="append(5)">5</div>
         <div class="btn" @click="append(6)">6</div>
-        <div class="btn operator">X</div>
+        <div class="btn operator" @click="multiply">X</div>
         <div class="btn" @click="append(1)">1</div>
         <div class="btn" @click="append(2)">2</div>
         <div class="btn" @click="append(3)">3</div>
-        <div class="btn operator">-</div>
+        <div class="btn operator" @click="subtract">-</div>
         <div class="btn" @click="append(0)">0</div>
         <div class="btn" @click="dot">.</div>
         <div class="btn">=</div>
-        <div class="btn operator">+</div>
+        <div class="btn operator" @click="sum">+</div>
     </div>
 </template>
 
@@ -51,7 +51,15 @@ export default {
             this.operator = (a, b) => b/a; 
         },
         multiply(){
-            this.operator = (a, b) => b*a;
+            this.operator = (a, b) => a*b;
+            
+        },
+        subtract(){
+            this.operator = (a, b) => a-b;
+            
+        },
+        sum(){
+            this.operator = (a, b) => a+b;
             
         }
     }
